2a9f1acbb4f806d395c7d66e688fee566c576651
Loc Nguyen <locn562836@gmail.com>
2024-12-11 19:40:15 +0000

    fix auth

--- pages/Auth/jsobjects/registerObject/registerObject.js
+++ pages/Auth/jsobjects/registerObject/registerObject.js
@@ -1,7 +1,13 @@
 export default {
 
 	async signUp () { 
-		let user_profile = register_user.run();
+		let user_profile = null;
+		try {
+			user_profile = await register_user.run();
+		} catch (e) {
+			showAlert("Đã có lỗi xảy ra khi đăng ký", "error");
+			return;
+		}
 
 		if(user_profile == null) {
 			showAlert("Lỗi đăng ký, vui lòng thử lại sau. ", "error")
